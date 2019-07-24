f94baec7fa92a9a20a95093bab121b40c02bb514
jingua1234 <285152739@qq.com>
2019-07-24 11:15:55 +0800

    检查机列表和eqpID修改

--- src/renderer/views/settings/inspection-list.vue
+++ src/renderer/views/settings/inspection-list.vue
@@ -22,7 +22,7 @@
         align="center"
         label="IP">
         <template slot-scope="scope">
-          <span>{{ scope.row.ip }}</span>
+          <span>{{ scope.row.ipAddress }}</span>
         </template>
       </el-table-column>
 
@@ -30,7 +30,7 @@
         width="130px"
         label="EQP Name">
         <template slot-scope="scope">
-          <span>{{ scope.row.eqp_name }}</span>
+          <span>{{ scope.row.eqpName }}</span>
         </template>
       </el-table-column>
 
@@ -40,7 +40,7 @@
         <template slot-scope="{row}">
           <template v-if="row.edit">
             <el-input
-              v-model="row.eqp_id"
+              v-model="row.eqpId"
               class="edit-input"
               size="small" />
             <el-button
@@ -52,7 +52,7 @@
               cancel
             </el-button>
           </template>
-          <span v-else>{{ row.eqp_id }}</span>
+          <span v-else>{{ row.eqpId }}</span>
         </template>
       </el-table-column>
 
@@ -61,16 +61,16 @@
         label="Port Num"
         width="110">
         <template slot-scope="scope">
-          <span>{{ scope.row.port_num }}</span>
+          <span>{{ scope.row.portNum }}</span>
         </template>
       </el-table-column>
 
       <el-table-column
         width="100px"
         align="center"
-        label="Use">
+        label="Used">
         <template slot-scope="scope">
-          <span>{{ scope.row.use }}</span>
+          <span>{{ scope.row.used }}</span>
         </template>
       </el-table-column>
 
@@ -79,7 +79,7 @@
         align="center"
         label="UpdateTime">
         <template slot-scope="scope">
-          <span>{{ scope.row.update_time | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          <span>{{ scope.row.updateTime | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
       </el-table-column>
 
@@ -91,156 +91,63 @@
           <el-button
             v-if="row.edit"
             type="success"
-            size="mini"
+            size="small"
+            icon="el-icon-circle-check-outline"
             @click="confirmEdit(row)">
             Ok
           </el-button>
           <el-button
             v-else
             type="primary"
-            size="mini"
+            size="small"
+            icon="el-icon-edit"
             @click="row.edit=!row.edit">
             Edit
-          </el-button>
-          <el-button
-            type="primary"
-            size="mini"
-            @click="handleCreate">
-            Add
           </el-button>
         </template>
       </el-table-column>
     </el-table>
-
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.limit"
-      @pagination="getList" />
-
-    <el-dialog
-      :title="textMap[dialogStatus]"
-      :visible.sync="dialogFormVisible">
-      <el-form
-        ref="dataForm"
-        :rules="rules"
-        :model="temp"
-        label-position="left"
-        label-width="100px"
-        style="width: 400px; margin-left:50px;">
-        <el-form-item
-          label="IP"
-          prop="ip">
-          <el-input v-model="temp.ip" />
-        </el-form-item>
-        <el-form-item
-          label="EQP Name"
-          prop="eqp_name">
-          <el-input v-model="temp.eqp_name" />
-        </el-form-item>
-        <el-form-item
-          label="EQP ID"
-          prop="eqp_id">
-          <el-input v-model="temp.eqp_id" />
-        </el-form-item>
-
-        <el-form-item
-          label="Port Num"
-          prop="port_num">
-          <el-input v-model.number="temp.port_num" />
-        </el-form-item>
-        <el-form-item
-          label="Use"
-          prop="use">
-          <el-input v-model.number="temp.use" />
-        </el-form-item>
-      </el-form>
-      <div
-        slot="footer"
-        class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">
-          Cancel
-        </el-button>
-        <el-button
-          type="primary"
-          @click="dialogStatus==='create'?createData():updateData()">
-          OK
-        </el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getInspectionLists, createInspection } from '@/api/settings'
-import Pagination from '@/components/Pagination'
+import { getInspectionLists, UpdateInspMachineEQPID } from '@/api/settings'
 
 export default {
-  name: 'InlineEditTable',
-  components: { Pagination },
   data() {
     return {
       tableKey: 0,
-      // list: null,
-      total: 0,
-      listLoading: true,
-      listQuery: {
-        page: 1,
-        limit: 10,
-        sort: '+index'
-      },
-      temp: {
-        index: undefined,
-        ip: '',
-        eqp_name: '',
-        eqp_id: '',
-        port_num: '',
-        use: '',
-        update_time: new Date()
-      },
-      dialogFormVisible: false,
-      dialogStatus: '',
-      textMap: {
-        update: 'Edit',
-        create: 'Create'
-      },
-      dialogPvVisible: false,
-      rules: {
-        ip: [{ required: true, message: 'IP is required' }, { min: 7, max: 15, message: '长度在 7 到 15 个字符', trigger: 'blur' }],
-        eqp_name: [{ required: true, message: 'EQP Name is required' }, { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }],
-        eqp_id: [{ required: true, message: 'EQP ID is required' }, { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }],
-        port_num: [{ required: true, message: 'Port Number is required', trigger: 'blur' },
-          { type: 'number', message: 'Use must be a number', trigger: 'blur' }],
-        use: [{ required: true, message: 'Use is required' },
-          { type: 'number', message: 'Use must be a number', trigger: 'blur' }]
-      }
+      list: [],
+      listLoading: true
+
     }
   },
   created() {
     this.getList()
   },
   methods: {
-    async getList() {
+    getList: function() {
       this.listLoading = true
-      const { data } = await getInspectionLists(this.listQuery)
-      const { items } = data
-      this.list = items.map(v => {
-        this.$set(v, 'edit', false) // https://vuejs.org/v2/guide/reactivity.html
-        v.originalID = v.eqp_id //  will be used when user click the cancel botton
-        return v
-      })
-      getInspectionLists(this.listQuery).then(response => {
-        this.total = response.data.total
-        // Just to simulate the time of the request
+      getInspectionLists().then(response => {
+        const data = response
+        const items = data.machines
+        this.list = items.map(v => {
+          this.$set(v, 'edit', false) // https://vuejs.org/v2/guide/reactivity.html
+          v.originalID = v.eqpId //  will be used when user click the cancel botton
+          return v
+        })
+        this.listLoading = false
         setTimeout(() => {
           this.listLoading = false
         }, 1.5 * 1000)
-      })
-      this.listLoading = false
+      }
+      )
+        .catch(function(error) {
+          console.log(error)
+        })
     },
     cancelEdit(row) {
-      row.eqp_id = row.originalID
+      row.eqpId = row.originalID
       row.edit = false
       this.$message({
         message: 'The EQP ID has been restored to the original value',
@@ -248,46 +155,21 @@ export default {
       })
     },
     confirmEdit(row) {
-      row.edit = false
-      row.originalID = row.eqp_id
-      this.$message({
-        message: 'The EQP ID has been edited',
-        type: 'success'
-      })
-    },
-    resetTemp() {
-      this.temp = {
-        index: undefined,
-        ip: '',
-        eqp_name: '',
-        eqp_id: '',
-        port_num: '',
-        use: '',
-        update_time: new Date()
-      }
-    },
-    handleCreate() {
-      this.resetTemp()
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs.dataForm.clearValidate()
-      })
-    },
-    createData() {
-      this.$refs.dataForm.validate((valid) => {
-        if (valid) {
-          createInspection(this.temp).then(() => {
-            this.list.unshift(this.temp)
-            this.dialogFormVisible = false
-            this.$notify({
-              title: '成功',
-              message: '创建成功',
-              type: 'success',
-              duration: 2000
-            })
-          })
+      const InspectionMachine = { eqpName: row.eqpName, eqpId: row.eqpId }
+      UpdateInspMachineEQPID(InspectionMachine).then(() => {
+        for (const v of this.list) {
+          if (v.eqpId === row.originalID) {
+            const index = this.list.indexOf(v)
+            this.list.splice(index, 1, row.eqpId)
+            break
+          }
         }
+        row.edit = false
+        row.originalID = row.eqpId
+        this.$message({
+          message: 'The EQP ID has been edited',
+          type: 'success'
+        })
       })
     }
   }
