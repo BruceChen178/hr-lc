4098bc9a4facbba2ba38b310a47fae33d3a214b8
jingua1234 <285152739@qq.com>
2019-07-29 13:16:31 +0800

    检查机列表和eqpID修改

--- src/renderer/views/io/components/io-section.vue
+++ src/renderer/views/io/components/io-section.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="app-container">
-    <el-carousel
-      direction="vertical"
-      :autoplay="false">
-      <el-carousel-item
-        v-for="item in sections"
-        :key="item.id">
-        <!--      <h3 class="medium">{{ item }}</h3>-->
-        <i-o-table
-          :datasource-id="item.datasourceId"
-          :dev-type="item.devType"
-          :start-address="item.startPoint"
-          :direction="item.direction ===undefined? 0: item.direction"
-          :addr-len="item.length"
-          :comment="item.comment" />
-      </el-carousel-item>
-    </el-carousel>
+    <el-tabs :tab-position="tabPosition" style="">
+      <el-tab-pane v-for="item in sections" :key="item.id" :label="item.label">
+<!--        <split-pane split="vertical" @resize="resize">-->
+<!--          <template slot="paneL">-->
+<!--            <div class="left-container">-->
+          <div class="table-container">
+        <el-row :gutter="20">
+          <el-col :span="6" :offset="4"><span>Receive Data (PLC >> CIM)</span></el-col>
+          <el-col :span="6" :offset="6"><span>Send Data (PLC << CIM)</span></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+              <i-o-table
+                      :datasource-id="item.datasourceId"
+                      :dev-type="item.devType"
+                      :start-address="item.startPoint"
+                      :direction='0'
+                      :addr-len="item.length"
+                      :comment="item.comment" />
+          </el-col>
+<!--            </div>-->
+<!--          </template>-->
+<!--          <template slot="paneR">-->
+<!--            <div class="right-container">-->
+          <el-col :span="12">
+              <i-o-table
+                      :datasource-id="item.datasourceId"
+                      :dev-type="item.devType"
+                      :start-address="item.startPoint"
+                      :direction="dir"
+                      :addr-len="item.length"
+                      :comment="item.comment" />
+          </el-col>
+        </el-row>
+          </div>
+          <!--            </div>-->
+<!--          </template>-->
+<!--        </split-pane>-->
+<!--    <el-carousel-->
+<!--      direction="vertical"-->
+<!--      :autoplay="false">-->
+<!--      <el-carousel-item-->
+<!--        v-for="item in sections"-->
+<!--        :key="item.id">-->
+<!--              <h3 class="medium">{{ item }}</h3>-->
+
+      </el-tab-pane>
+    </el-tabs>
+<!--      </el-carousel-item>-->
+<!--    </el-carousel>-->
   </div>
 </template>
 
@@ -41,7 +75,9 @@ export default {
         limit: 50,
         sort: '',
         currentDS: ''
-      }
+      },
+      dir: 'FROM_CIM',
+      tabPosition: 'top'
     }
   },
   created() {
@@ -52,8 +88,26 @@ export default {
       this.query.currentDS = this.sourceId
       getMitPLCConfigs(this.query)
         .then(response => {
-          this.sections = response.configs
+          const items = response.configs
+          const len = items.length
+
+          if (len > 1) {
+            // items[0]['label'] = 'Bit'
+            // items[1]['label'] = 'Word'
+            let i
+            for (i = 0; i < len; i++) {
+              if (items[i].devType === 'B') {
+                items[i]['label'] = 'Bit'
+              } else if (items[i].devType === 'W') {
+                items[i]['label'] = 'Word'
+              }
+            }
+
+            this.sections.push(items[0])
+            this.sections.push(items[1])
+          }
         })
+
         // eslint-disable-next-line func-names
         .catch(function(error) {
           console.log(error)
@@ -65,19 +119,5 @@ export default {
 </script>
 
 <style scoped>
-  .el-carousel__item i-o-table {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 600px;
-    margin: 0;
-  }
 
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
 </style>
--- src/renderer/views/io/components/io-table.vue
+++ src/renderer/views/io/components/io-table.vue
@@ -5,160 +5,42 @@
       border
       fit
       highlight-current-row>
-      <el-table-column
-        v-loading="loading"
-        align="center"
-        label="Start Addr"
-        width="65"
-        element-loading-text="Loading...">
-        <template slot-scope="scope">
-          <span style="color: #2c3e50">{{ scope.row.startAddr }}</span>
-        </template>
-      </el-table-column>
+<!--      <el-table-column-->
+<!--        v-loading="loading"-->
+<!--        align="center"-->
+<!--        label="Device"-->
+<!--        width="65"-->
+<!--        element-loading-text="Loading...">-->
+<!--        <template slot-scope="scope">-->
+<!--          <span style="color: #2c3e50">{{ scope.row.datatype }}</span>-->
+<!--        </template>-->
+<!--      </el-table-column>-->
 
       <el-table-column
         min-width="100px"
         align="center"
-        label="+0">
+        label="Address">
         <template slot-scope="{row}">
           <!--          <span>{{ row.address0.description }}</span>-->
-          <span>{{ row.address0.value }}</span>
+          <span>{{ row.sourceItemId }}</span>
         </template>
       </el-table-column>
 
       <el-table-column
         min-width="100px"
         align="center"
-        label="+1">
+        label="Comment">
         <template slot-scope="{row}">
-          <span>{{ row.address1.description }}</span>
-          <span>{{ row.address1.value }}</span>
+          <span>{{ row.description }}</span>
         </template>
       </el-table-column>
 
       <el-table-column
         min-width="100px"
         align="center"
-        label="+2">
+        label="Value">
         <template slot-scope="{row}">
-          <span>{{ row.address2.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+3">
-        <template slot-scope="{row}">
-          <span>{{ row.address3.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+4">
-        <template slot-scope="{row}">
-          <span>{{ row.address4.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+5">
-        <template slot-scope="{row}">
-          <span>{{ row.address5.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+6">
-        <template slot-scope="{row}">
-          <span>{{ row.address6.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+7">
-        <template slot-scope="{row}">
-          <span>{{ row.address7.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+8">
-        <template slot-scope="{row}">
-          <span>{{ row.address8.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+9">
-        <template slot-scope="{row}">
-          <span>{{ row.address9.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+A">
-        <template slot-scope="{row}">
-          <span>{{ row.addressA.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+B">
-        <template slot-scope="{row}">
-          <span>{{ row.addressB.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+C">
-        <template slot-scope="{row}">
-          <span>{{ row.addressC.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+D">
-        <template slot-scope="{row}">
-          <span>{{ row.addressD.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+E">
-        <template slot-scope="{row}">
-          <span>{{ row.addressE.description }}</span>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        min-width="100px"
-        align="center"
-        label="+F">
-        <template slot-scope="{row}">
-          <span>{{ row.addressF.description }}</span>
+          <span>{{ row.value }}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -167,6 +49,7 @@
 
 <script>
 import { mapState } from 'vuex'
+import { getEQMetadataByDSId } from '../../../api/datasource'
 export default {
   name: 'IOTable',
   props: {
@@ -199,7 +82,7 @@ export default {
     return {
       query: {
         page: 1,
-        limit: 5,
+        limit: 500,
         type: this.currentDS,
         sort: ''
       },
@@ -207,7 +90,9 @@ export default {
       // updatingSourceItemId: '',
       // varValue: '',
       loading: false,
-      tableData: []
+      tableData: [],
+      devType: this.devType,
+      direction: this.direction
     }
   },
   computed: {
@@ -239,77 +124,144 @@ export default {
 
       // update tableData item value
       // find the item
-      for (let j = 0; j < this.tableData.length; j += 1) {
-        const tableRow = parseInt(j / 16)
-        const tableCol = `address${(j % 16).toString(16).toUpperCase()}`
-        if (parseInt(this.updatingSourceItemId.slice(1), 16) === parseInt(this.tableData[tableRow][tableCol].sourceItem.slice(1), 16)) {
-          this.tableData[tableRow][tableCol].value = this.varValue
-          break
-        }
-      }
+      // for (let j = 0; j < this.tableData.length; j += 1) {
+      //   const tableRow = parseInt(j / 16)
+      //   const tableCol = `address${(j % 16).toString(16).toUpperCase()}`
+      //   if (parseInt(this.updatingSourceItemId.slice(1), 16) === parseInt(this.tableData[tableRow][tableCol].sourceItem.slice(1), 16)) {
+      //     this.tableData[tableRow][tableCol].value = this.varValue
+      //     break
+      //   }
+      // }
+      this.tableData.values = this.varValue()
     }
+    // devType(newValue) {
+    //   this.query.currentDS = this.datasourceId
+    //   getEQMetadataByDSId(this.query).then(response => {
+    //     const all = response.data
+    //     const len = all.length
+    //     let i = 0
+    //     const selectIOData = []
+    //     for (; i < len; i++) {
+    //       if (this.newValue === 'B') {
+    //         if (this.direction === 0) {
+    //           if (all[i].datatype === 'BOOLEAN_TYPE' && all[i].direction === undefined) {
+    //             selectIOData.push(all[i])
+    //           }
+    //         } else if (this.direction === 1) {
+    //           if (all[i].datatype === 'BOOLEAN_TYPE' && all[i].direction === 1) {
+    //             selectIOData.push(all[i])
+    //           }
+    //         }
+    //       } else if (this.newValue === 'W') {
+    //         if (this.direction === 0) {
+    //           if (all[i].sourceItemId.includes('W') && all[i].direction === undefined) {
+    //             selectIOData.push(all[i])
+    //           }
+    //         } else if (this.direction === 1) {
+    //           if (all[i].sourceItemId.includes('W') && all[i].direction === 1) {
+    //             selectIOData.push(all[i])
+    //           }
+    //         }
+    //       }
+    //     }
+    //     this.tableData = selectIOData
+    //     console.log('watch success')
+    //   })
+    // }
   },
   created() {
     this.generateTemplate()
   },
   methods: {
     generateTemplate() {
-      if (this.addrLen > 0) {
-        let addrType = ''
-        if (this.devType.startsWith('B')) {
-          addrType = 'B'
-        } else if (this.devType.startsWith('W')) {
-          addrType = 'W'
-        } else {
-          return
-        }
+      // if (this.addrLen > 0) {
+      //   let addrType = ''
+      //   if (this.devType.startsWith('B')) {
+      //     addrType = 'B'
+      //   } else if (this.devType.startsWith('W')) {
+      //     addrType = 'W'
+      //   } else {
+      //     return
+      //   }
 
-        // generate table template
-        for (let i = 0; i < this.addrLen; i += 1) {
-          const obj = {}
-          const hex = parseInt(this.startAddress, 16)
-          const intRow = hex + i * 16
-          obj.startAddr = addrType + (hex + i * 16).toString(16).toUpperCase()
-          // eslint-disable-next-line no-plusplus
-          for (let j = 0; j < 16; j++) {
-            const intCol = intRow + j
-            // get meta data
-            const descVal = 'Reserved'
-            const varVal = '99'
-            // if (currMetadatas !== undefined) {
-            //   for (const item of currMetadatas) {
-            //     if (parseInt(item.sourceItemId.slice(1), 16) === intCol) {
-            //       descVal = item.description // item.abbrv
-            //       varVal = item.varValue
-            //       break
-            //     }
-            //   }
-            // }
-            obj[`address${j.toString(16).toUpperCase()}`] = { sourceItem: `${addrType + intCol.toString(16).toUpperCase()}`, description: descVal, value: varVal }
-          }
-          this.tableData.push(obj)
-        }
-        // fill available/used item
-        // fill
-        const all = this.$store.getters.allMetadatas
-        // var currMetadatas = []
-        // eslint-disable-next-line no-plusplus
-        for (let i = 0; i < all.length; i++) {
-          if (all[i].datasourceId === this.datasourceId && all[i].direction === this.direction && all[i].sourceItemId.slice(0, 1) === this.devType) {
-            // currMetadatas.push(all[i])
-            // find the item
-            for (let j = 0; j < this.tableData.length; j += 1) {
-              const tableRow = parseInt(j / 16)
-              const tableCol = `address${(j % 16).toString(16).toUpperCase()}`
-              if (parseInt(all[i].sourceItemId.slice(1), 16) === parseInt(this.tableData[tableRow][tableCol].sourceItem.slice(1), 16)) {
-                this.tableData[tableRow][tableCol].description = all[i].description // item.abbrv
-                this.tableData[tableRow][tableCol].value = all[i].varValue
-                break
+      // generate table template
+      // for (let i = 0; i < this.addrLen; i += 1) {
+      //   const obj = {}
+      //   const hex = parseInt(this.startAddress, 16)
+      //   const intRow = hex + i * 16
+      //   obj.startAddr = addrType + (hex + i * 16).toString(16).toUpperCase()
+      //   // eslint-disable-next-line no-plusplus
+      //   for (let j = 0; j < 16; j++) {
+      //     const intCol = intRow + j
+      //     // get meta data
+      //     const descVal = 'Reserved'
+      //     const varVal = '99'
+      //     // if (currMetadatas !== undefined) {
+      //     //   for (const item of currMetadatas) {
+      //     //     if (parseInt(item.sourceItemId.slice(1), 16) === intCol) {
+      //     //       descVal = item.description // item.abbrv
+      //     //       varVal = item.varValue
+      //     //       break
+      //     //     }
+      //     //   }
+      //     // }
+      //     obj[`address${j.toString(16).toUpperCase()}`] = { sourceItem: `${addrType + intCol.toString(16).toUpperCase()}`, description: descVal, value: varVal }
+      //   }
+      //   this.tableData.push(obj)
+      // }
+      // fill available/used item
+      // fill
+      this.query.currentDS = this.datasourceId
+      getEQMetadataByDSId(this.query).then(response => {
+        const all = response.data
+        const len = all.length
+        let i = 0
+        const selectIOData = []
+        for (; i < len; i++) {
+          if (this.devType === 'B') {
+            if (this.direction === 0) {
+              if (all[i].datatype === 'BOOLEAN_TYPE' && all[i].direction === undefined) {
+                selectIOData.push(all[i])
+              }
+            } else if (this.direction === 'FROM_CIM') {
+              if (all[i].datatype === 'BOOLEAN_TYPE' && all[i].direction === 'FROM_CIM') {
+                selectIOData.push(all[i])
+              }
+            }
+          } else if (this.devType === 'W') {
+            if (this.direction === 0) {
+              if (all[i].sourceItemId.includes('W') && all[i].direction === undefined) {
+                selectIOData.push(all[i])
+              }
+            } else if (this.direction === 'FROM_CIM') {
+              if (all[i].sourceItemId.includes('W') && all[i].direction === 'FROM_CIM') {
+                selectIOData.push(all[i])
               }
             }
           }
         }
-      }
+        this.tableData = selectIOData
+      })
+
+      // const all = this.$store.getters.allMetadatas
+      // this.tableData = all
+      // var currMetadatas = []
+      // eslint-disable-next-line no-plusplus
+      // for (let i = 0; i < all.length; i++) {
+      //   if (all[i].datasourceId === this.datasourceId && all[i].direction === this.direction && all[i].sourceItemId.slice(0, 1) === this.devType) {
+      //     // currMetadatas.push(all[i])
+      //     // find the item
+      //     for (let j = 0; j < this.tableData.length; j += 1) {
+      //       const tableRow = parseInt(j / 16)
+      //       const tableCol = `address${(j % 16).toString(16).toUpperCase()}`
+      //       if (parseInt(all[i].sourceItemId.slice(1), 16) === parseInt(this.tableData[tableRow][tableCol].sourceItem.slice(1), 16)) {
+      //         this.tableData[tableRow][tableCol].description = all[i].description // item.abbrv
+      //         this.tableData[tableRow][tableCol].value = all[i].varValue
+      //         break
+      //       }
+      //     }
+      //   }
+      // }
     }
   }
 }
